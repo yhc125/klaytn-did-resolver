de0ea39dac82cf6c9ea82a378b27d845b9601954
yhc125 <yhc125r@naver.com>
2023-04-19 15:48:12 +0900

    update: KlaytnDIDResolverOptions에 contractAddress 추가

--- src/klaytn-did-resolver.ts
+++ src/klaytn-did-resolver.ts
@@ -4,7 +4,6 @@ import Caver, { PublicKeyForRPC } from 'caver-js';
 export interface KlaytnDIDResolverOptions {
   rpcUrl: string;
 	network: 'baobab' | 'cypress';
-	contractAddress: string;
 }
 
 export function getResolver(options: KlaytnDIDResolverOptions): Record<string, DIDResolver> {
@@ -15,12 +14,10 @@ export function getResolver(options: KlaytnDIDResolverOptions): Record<string, D
 export class KlaytnDIDResolver {
   private caver: Caver;
 	private network: 'baobab' | 'cypress';
-	private contractAddress: string;
 
   constructor(options: KlaytnDIDResolverOptions) {
     this.caver = new Caver(options.rpcUrl);
 		this.network = options.network;
-		this.contractAddress = options.contractAddress;
   }
 
 	async resolve(did: string): Promise<DIDResolutionResult> {
@@ -98,7 +95,7 @@ export class KlaytnDIDResolver {
 	private async isDeactivated(address: string): Promise<boolean> {
     // Replace the following line with the actual call to the smart contract
     const deactivated = await this.caver.rpc.klay.call({
-      to: this.contractAddress, // KlaytnDIDRegistryAddress '0xf7C4A040d44cA56C3cc8FAAA3A801b89DD936671'
+      to: '0xf7C4A040d44cA56C3cc8FAAA3A801b89DD936671', // KlaytnDIDRegistryAddress
       data: this.caver.abi.encodeFunctionCall({
         name: 'isDeactivated',
         type: 'function',
--- src/test/klaytn-did-resolver.test.ts
+++ src/test/klaytn-did-resolver.test.ts
@@ -11,13 +11,11 @@ describe('klaytn-did-resolver', () => {
     const baobabConfig: KlaytnDIDResolverOptions = {
       rpcUrl: 'https://api.baobab.klaytn.net:8651',
       network: 'baobab',
-			contractAddress: '0xf7C4A040d44cA56C3cc8FAAA3A801b89DD936671',
     };
 
     const cypressConfig: KlaytnDIDResolverOptions = {
       rpcUrl: 'https://api.cypress.klaytn.net:8651',
       network: 'cypress',
-			contractAddress: '',
     };
 
     const baobabDIDResolver = getKlaytnDIDResolver(baobabConfig);
